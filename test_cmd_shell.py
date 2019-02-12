0ca84afa6bc32e9da10d8c0faed9c75ba5fda092
cadeluca <cadeluca@uvm.edu>
2019-02-11 19:08:19 -0500

    Did some file restructuring and added new commands and preparations for new command implementation.

--- test_cmd_shell.py
+++ test_cmd_shell.py
@@ -9,7 +9,9 @@ debug = True
 
 # TODO: ask Hibbeler if citations are necessary from SQLite official tutorial
 
-
+#
+# Helper functions
+#
 def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by the db_file
@@ -23,8 +25,14 @@ def create_connection(db_file):
         print(e)
     return None
 
-
-def show_tables():
+def find_matching_show(searched_string):
+    # do some regex here
+    results = []
+    return results
+#
+# SQL style functions
+#
+def list_tables():
     """ queries database for all tables and
         displays the table names in a column
     """
@@ -51,7 +59,7 @@ def show_schema():
     conn.commit()
 
 
-def show_table_layout(inp):
+def list_table_content(inp):
     """ queries database for a matching table to inp
         then displays the table headers and content
     :param inp: user inputted argument
@@ -80,18 +88,26 @@ def show_table_layout(inp):
 
     conn.commit()
 
-# DO NOT DELETE.
-# def multi_param(inp):
-#     if inp == 'first option':
-#
-#     elif inp == 'second option':
-#         #
-#     else:
-#         print("Unrecognized or missing parameters. Usage:"
-#               "\n\t- show \"table_name\" to display that table.")
+
+def list_columns(desired_table_name):
+    """ queries database for a matching table and displays
+        the columns that are a part of that table if it exists
+    :param desired_table_name desired table to fetch columns for
+    """
+    cur = conn.cursor()
+    desired_table = cur.execute('PRAGMA table_info(%s)' % desired_table_name).fetchall()
+    print(desired_table)
+    if len(desired_table) != 0:
+        print("The table '%s' has the following columns:" % desired_table_name)
+        for column in desired_table:
+            print("\t" + column[1])
+    else:
+        print("The table '%s' is not in the database." % desired_table_name)
+
+    conn.commit()
 
 
-def simple_column_select(query_list):
+def full_column_return(query_list):
     """ queries database for a matching table and column
         then displays the requested column from the table
     :param query_list list containing column and table
@@ -110,65 +126,117 @@ def simple_column_select(query_list):
     conn.commit()
 
 
+#
+# End SQL style functions
+#
+
+#
+# Simple grammar functions
+#
+
+# the following functions will be given a list of possible matching shows from prompt
+
+def detail_viewer(detail_type, show_list):
+    cur = conn.cursor()
+    if detail_type == 'details':
+        # give all information on that show
+        # this could be a good spot for a join
+        cur.execute("......................................")
+    else:
+        grammar_dict = {"runtime": "corresponding runtime id", "genre": "", "network": "", "seasons": "", "status": ""}
+        detail = grammar_dict[detail_type]
+        table = "" # this should be the table we get this info from
+        if len(show_list) == 1:
+            # show detail is
+            # do sql
+            print("")
+        else:
+            for show in show_list:
+                # select <runtime> from <table that has runtime> where <name id> = <name>
+                result = cur.execute("SELECT %s FROM %s WHERE nameidtobechanged = '%s'" % (detail, table, show)).fetchone()
+
+#
+# End Simple grammar functions
+#
+
+
+#
+# Complex grammar functions
+#
+
+# TODO @cadeluca: complete this @ other todo
+def show_finder(search_request_list):
+    print(search_request_list)
+    # request_list reads like:   look for a show
+
+#
+# End Complex grammar functions
+#
+
+
+
 class MainPrompt(Cmd):
-    prompt = '<tvTuner>'
+    prompt = '<tvTuner> '
 
     def do_exit(self, inp):
-        print("Bye")
+        cli_animations.typewrite("Tune in next time!", 0.04, 0.04)
         return True
 
-    def do_greet(self, inp):
-        # typewrite("Welcome to tvTuner!", 0.04, 1)
-        print('Hi!')
-
-    def help_greet(self):
-        print('the console says hello!')
-
     def help_exit(self):
-        print('exit the application. Shorthand: x q Ctrl-D.')
+        print('Exits the application. Shorthand: `x` `q` `Ctrl-D`.')
 
     def do_tables(self, inp):
-        show_tables()
+        list_tables()
 
     def do_schema(self, inp):
         show_schema()
 
-    def do_spin(self, inp):
-        cli_animations.spin_animation()
+    def do_list(self, inp):
+        list_table_content(inp)
 
-    def do_display(self, inp):
-        show_table_layout(inp)
-
-    def do_hello(self, s):
-        if s == '':
-            s = input('Your name please: ')
-        print('Hello', s)
+    def do_runtime(self, inp):
+        # do regex search for shows, return a list
+        show_list = find_matching_show(inp)
+        detail_viewer('runtime', show_list)
 
     def do_get_column(self, inp):
         params = inp.split()
-        if len(params) != 2:
-            print("Invalid number of arguments.\nUsage:\n\tget_column 'table' 'column'")
-            return
+        if len(params) == 2:
+            full_column_return(params)
+        elif len(params) == 1:
+            list_columns(params[0])
         else:
-            simple_column_select(params)
+            print("Invalid number of arguments.\nUsage:"
+                  "\n\tget_column 'table' - returns a list of columns in that table."
+                  "\n\tget_column 'table' 'column' - returns the contents of that column from that table.")
+            return
+
+    # TODO: ASK HIBBELER IF THIS COUNTS AS USING THE GRAMMAR
+    # TODO @cadeluca: continue to work on this
+    def do_find(self, inp):
+        if inp == '':
+            print("What do you want to find? Enter the first character or the word to search:")
+            print("\t[1] tracks\t[2] albums\n\t[3] artists\t[4] genres\n\t[5] cancel")
+            valid_search = False
+            search_request = []
+            valid_options = ['1', '2', '3', '4', '5', 'tracks', 'albums', 'artists', 'genres', 'cancel']
+            while not valid_search:
+                try:
+                    search_type = input('- find: ')
+                except ValueError:
+                    print("that is not one of the options")
+                    continue
+                if search_type not in valid_options:
+                    print("that is not one of the options")
+                    continue
+                else:
+                    valid_search = True
+            print("woo! here we would link to the valid search for each type, by calling the appropriate function")
 
     def default(self, inp):
         if inp == 'x' or inp == 'q':
             return self.do_exit(inp)
         print("Unrecognized command: {}".format(inp))
-
-    # example of multi params in just the command
-    def do_add(self, s):
-        l = s.split()
-        if len(l) != 2:
-            print("*** invalid number of arguments")
-            return
-        try:
-            l = [int(i) for i in l]
-        except ValueError:
-            print("*** arguments should be numbers")
-            return
-        print(l[0] + l[1])
 
 
 if __name__ == '__main__':
@@ -180,3 +248,36 @@ if __name__ == '__main__':
         if debug:
             print("connected!")
         MainPrompt().cmdloop(cli_animations.intro())
+
+
+# extras lol
+
+# DO NOT DELETE.
+# def multi_param(inp):
+#     if inp == 'first option':
+#
+#     elif inp == 'second option':
+#         #
+#     else:
+#         print("Unrecognized or missing parameters. Usage:"
+#               "\n\t- show \"table_name\" to display that table.")
+
+
+ # # example of multi params in just the command
+    # def do_add(self, s):
+    #     l = s.split()
+    #     if len(l) != 2:
+    #         print("*** invalid number of arguments")
+    #         return
+    #     try:
+    #         l = [int(i) for i in l]
+    #     except ValueError:
+    #         print("*** arguments should be numbers")
+    #         return
+    #     print(l[0] + l[1])
+
+    # example of interacting with the shell, we could use this style to
+    # def do_hello(self, s):
+    #     if s == '':
+    #         s = input('Your name please: ')
+    #     print('Hello', s)
