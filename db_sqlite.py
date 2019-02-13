3ae5be81f8ed46a92c9ea5a8f8359635f68e754e
mdoyle3 <mdoyle3@uvm.edu>
2019-02-13 16:11:39 -0500

    database added

---
+++ db_sqlite.py
@@ -0,0 +1,19 @@
+# http://www.sqlitetutorial.net/sqlite-python/creating-database/
+
+import sqlite3
+from sqlite3 import Error
+
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+
+
+if __name__ == '__main__':
+    create_connection("tv_tuner.db")
