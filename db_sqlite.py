86c9bf239c67def66c70fffd0d163a76cc2a19f4
mdoyle3 <mdoyle3@uvm.edu>
2019-02-15 14:46:01 -0500

    added code to populate the database, just can't figure out syntax error (there isn't one but it won't run?) and have to connect the two (actually filling our database)

--- db_sqlite.py
+++ db_sqlite.py
@@ -1,7 +1,23 @@
 # http://www.sqlitetutorial.net/sqlite-python/creating-database/
+# https://stackoverflow.com/questions/2887878/importing-a-csv-file-into-a-sqlite3-database-table-using-python
 
 import sqlite3
 from sqlite3 import Error
+import csv
+
+con = sqlite3.connect(":memory:")
+cur = con.cursor()
+cur.execute("CREATE TABLE t (Show name, Network, Runtime in minutes, Seasons, On/Off air, Genre);")
+
+with open('tv_tuner_data.csv') as fin:
+    # csv.DictReader uses first line in file for column headings by default
+    dr = csv.DictReader(fin) # comma is default delimiter
+    to_db = [(i['Show name'], i['Network'], i['Runtime in minutes'], i['Seasons'], i['On/Off air'], i['Genre'])
+             for i in dr]
+
+cur.executemany("INSERT INTO t (col1, col2) VALUES (?, ?);", to_db)
+con.commit()
+con.close()
 
 
 def create_connection(db_file):
